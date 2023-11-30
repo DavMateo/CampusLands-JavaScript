22e6c92b421d4f6bafe8bc5e07191633c2499a1d
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-11-29 21:39:59 -0500

    JavaScript inició potente xD

---
+++ temasClases/01. Introducción/miscript.js
@@ -0,0 +1,15 @@
+console.log("      *   ***** ");
+console.log("      *  *     *");
+console.log("      *  *      ");
+console.log("      *   ***** ");
+console.log("*     *        *");
+console.log("*     *  *     *");
+console.log(" *****    ***** ");
+
+
+console.log("Hola mundo :D");
+
+body = document.querySelector("body");
+
+document.write("<h2>Probando</h2>");
+body.innerHTML += "<h2>Probandooooo</h2>";
