185df0d741f204d531a4dfd24f1880499826e098
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-12-02 01:55:42 -0500

    .

---
+++ docs/js/data.js
@@ -0,0 +1,32 @@
+const categorias = {
+    practicas: {
+        metadata: {
+            imagen: '',
+            fondo: '',
+        }
+    },
+    proyecto: {
+        metadata: {
+            imagen: '',
+            fondo: '',
+        }
+    },
+    refuerzo: {
+        metadata: {
+            imagen: '',
+            fondo: '',
+        }
+    },
+    tareas: {
+        metadata: {
+            imagen: '',
+            fondo: '',
+        }
+    },
+    temasClases: {
+        metadata: {
+            imagen: '',
+            fondo: '',
+        }
+    },
+}
