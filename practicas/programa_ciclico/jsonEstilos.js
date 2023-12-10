8ddf1b51e847537dcb15b6c8bf7c3f7854760963
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-12-09 21:27:58 -0500

    Práctica solucionada (Falta CSS)

---
+++ practicas/programa_ciclico/jsonEstilos.js
@@ -0,0 +1,16 @@
+const jsonEstilosCss = {
+    "*": [
+            "padding: 0",
+            "margin: 0",
+            "box-sizing: border-box"
+        ],
+
+    "body": [
+            "background-color: #00111c;", 
+            "color: #e0f2fe;", 
+            "font-family: Arial, Helvetica, sans-serif",
+            "font-size: 16px"
+        ]
+};
+
+export { jsonEstilosCss };
