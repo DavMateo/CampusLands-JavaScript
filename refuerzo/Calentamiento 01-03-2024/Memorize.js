2c756839b8de25d306464f35dc965015c6337a03
DavMateo <davidcarrenomc@gmail.com>
2024-01-04 02:31:43 -0500

    .

---
+++ refuerzo/Calentamiento 01-03-2024/Memorize.js
@@ -0,0 +1,28 @@
+// DECLARANDO LAS VARIABLES GLOBALES NECESARIAS
+let count = 0, fnNameCopy, valuesCopy, strAnalizar = [];
+
+// DECLARANDO LAS FUNCIONES PRINCIPALES
+function memoized(fnName, values, arrTest) {
+    fnNameCopy = fnName;
+    const strArrayAnalizar = JSON.stringify(strAnalizar);
+    const strValues = JSON.stringify(values);
+
+    strArrayAnalizar.indexOf(strValues) !== -1 && fnName === fnNameCopy ? "" : count++;
+    strAnalizar.push(values);
+
+    return fnName === "sum" ? values[0] + values[1] : fnName === "fib" ? 0 : fnName === "factorial" ? 0 : "";
+}
+
+// DECLARANDO LA FUNCIÓN INICIALIZADORA
+function iniciarPrograma() {
+    const arrTest = [[2,2], [2,2], [], [1,2], [], [3,4], [2,2], [1,3], [], [2,1], []];
+    let arrResultado = [];
+
+    for (let i = 0; i < arrTest.length; i++) {
+        arrTest[i].length === 0 ? arrResultado.push(count) : arrResultado.push(memoized("sum", arrTest[i], arrTest));
+        //strAnalizar = [];
+    }
+    console.log(arrResultado);
+}
+
+iniciarPrograma();
