aa122f9cdc03605e573f8adbc43054ace4dea796
DavMateo <davidcarrenomc@gmail.com>
2024-01-20 15:33:01 -0500

    .

---
+++ refuerzo/Group By Function/groupByFunction.js
@@ -0,0 +1,17 @@
+// Definiendo la función callback
+let i = 0;
+
+function fn(item) {
+    console.log(item);
+    
+}
+
+
+const testArr = [
+    {"id": "1"},
+    {"id": "2"},
+    {"id": "3"}
+];
+
+
+Array.groupBy(fn(testArr[0]));
